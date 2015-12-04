c126f811e34204ad248649bffd4b11d6e20e6cd3
Mxm0320 <maximmoinat@gmail.com>
2015-12-04 15:57:38 +0100

    Included loading and plotting simple example

--- example_usage.py
+++ example_usage.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python
+"""
+Example of Petri Net Simulator usage
+"""
 
+import PetriNetSimulator
+
+# Initiate object
+pnSim = PetriNetSimulator()
+
+# Load the model from .pnml
+pnSim.parse_pnml_input_file( 'example_model.pnml' )
+
+# Execute 10 simulations of 100 steps (maximal parallel execution)
+pnSim.multi_sim(num_sim = 10, num_steps = 100)
+
+# Plot the time serie of each simulation trajectory of the CTNNB1 place
+pnSim.plotTimeSeries( 'CTNNB1', n_trajectories = 10 )
+# Plot the average with shaded standard deviation
+pnSim.plotAverageTimeSeries( 'CTNNB1' )
